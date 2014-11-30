f482389d1cdaaaaaa708d52031e977882247fac9
shk33 <m.coronel.seg@gmail.com>
2014-11-30 12:00:38 -0600

    Merge with Graficas

--- application/views/registrar_error.php
+++ application/views/registrar_error.php
@@ -1,117 +1,121 @@
-	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>  
-	<script type="text/javascript" src="<?php echo base_url()?>ckeditor/ckeditor.js"></script>
-
-	<h1>Registro de Resultados</h1>
-	<form id="options" action="<?php echo base_url()?>control_graficas" method="POST">
-		<br>
-		<button type="submit" value="Graficar" id="grafica_button" class="btn btn-primary btn-md">Graficar</button>
-	</form>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>  
+    <script type="text/javascript" src="<?php echo base_url()?>ckeditor/ckeditor.js"></script>
+</head>
+<body>
+    <h1>Bienvenido al sistema de registro y graficas</h1>
+    <h2>Opciones:</h2>
+    <form id="options" action="<?php echo base_url()?>control_graficas" method="POST">
+        <input type="submit" value="Graficar" id="grafica_button"/><br>
+        <input type="button" value="Regresar" id="back_button"/>
+    </form>
 
 <br>
-	<form id="registro_form">
-		<p>Fecha: <input id="datepicker" type="text" name="fecha"/></p>
-		<p>Seleccione una metrica</p>
-		<select id="select_metrica">
-			<option value="0" disabled="disabled" selected="selected">(Seleccionar)</option>
-			<option value="1">Sin uso del entorno virtual colaborativo</option>
-			<option value="2">Con uso del entorno virtual colaborativo</option>
-		</select><br>
-		<p>Escribe la ecuación</p>
-		<div id="select_ecuacion_placeholder">
-			<input type="text" name="ecuation">
-		</div>
-		<p>Numero de errores encontrados: <input type="number" min="1" max="1000" id="numero_errores"/></p>
-		<p>Lista de errores: <textarea id="lista_errores" cols="4" rows="4"></textarea></p>
-		<p>Numero de tipos de error encontrados: <input type="number" min="1" max="1000" id="numero_tipos_error"/></p>
-		<p>Lista de tipos de error: <textarea id="lista_tipos_error" cols="4" rows="4"></textarea></p>
+    <h2>Registro de informacion: </h2>
+    <form id="registro_form" action="" method="post">
+        <p>Fecha: <input id="datepicker" type="text" name="fecha" required/></p>
+        <p>Seleccione una metrica</p>
+        <select id="select_metrica">
+            <option value="0" disabled="disabled" selected="selected">(Seleccionar)</option>
+            <option value="1">Sin uso del entorno virtual colaborativo</option>
+            <option value="2">Con uso del entorno virtual colaborativo</option>
+        </select><br>
+        <p>Escriba la ecuacion evaluada <input type="text" id="ecuacion_evaluada"/></p>
+        <div id="select_ecuacion_placeholder"></div>
+        <p>Numero de errores encontrados: <input type="number" min="1" max="1000" id="numero_errores" required/></p>
+        <p>Lista de errores: <textarea id="lista_errores" cols="4" rows="4"></textarea></p>
+        <p>Numero de tipos de error encontrados: <input type="number" min="1" max="1000" id="numero_tipos_error" required/></p>
+        <p>Lista de tipos de error: <textarea id="lista_tipos_error" cols="4" rows="4"></textarea></p>
 
-		<!-- <input type="button" value="Guardar datos" id="submit_data"/> -->
-		<button type="button" id="submit_data" class="btn btn-primary btn-md">Guardar Datos</button>
-		<form id="options" action="<?php echo base_url()?>control_graficas" method="POST">
-			<button type="submit" value="Graficar" id="grafica_button" class="btn btn-primary btn-md">Graficar</button>
-		</form>
-	</form>
+    <input type="submit" value="Guardar datos" id="submit_data"/>
+    </form>
+</body>
 
-	<script type="text/javascript">
-		//Convert value to datepicker
-		$('#datepicker').datepicker();
-		//Cambiando con ckeditor
-		CKEDITOR.replace("lista_errores");
-		CKEDITOR.replace("lista_tipos_error");
-		CKEDITOR.config.width = 700
-		//Generar opciones para las ecuaciones
-		crearOpcionEcuaciones();
+    <script type="text/javascript">
+    $(document).on ('click', '#submit_data', function(e){
+        e.preventDefault();
+            var n_errors = $('#numero_errores').val();
+            var error_list = CKEDITOR.instances['lista_errores'].getData();
+            var n_type_error = $('#numero_tipos_error').val();
+            var type_list = CKEDITOR.instances['lista_tipos_error'].getData();
+            var date = $('#datepicker').datepicker({ dateFormat: 'dd-mm-yy' }).val();
+            //var num_ecuation = $('#select_ecuation').val();
+            var ecuation_ev = $('#ecuacion_evaluada').val();
+            var num_metrica =$('#select_metrica').val();
+            $.ajax({
+                type: "post",
+                url: '<?php echo base_url();?>registro_error/salvar_datos',
+                data: {
+                    fecha: date,
+                    num_errors: n_errors,
+                    list_error: error_list,
+                    num_type_errors: n_type_error,
+                    list_type_error: type_list,
+                    ecuation: ecuation_ev,
+                    metric: num_metrica
+                },
+                success: function (response){
+                    if (response == 1){
+                        alert ('Se guardo el registro correctamente');
+                        emptyAll();
+                    } else {
+                        alert ('ocurrio un error al guardar datos con el servidor');
+                        emptyAll();
+                    }
+                },
+                error: function (){
+                    alert('Error no esperado');
+                }
+            });
+    });
 
-		//AQUI VA EL CODIGO PARA REGRESAR O SALIR DEL SISTEMA
-		$('#back_button').click(function(){
-			alert('Regresando pero a la misma pagina');
-		});
-		$('#grafica_button').click(function(){
-			$.ajax({
-				type: "post",
-				url: '<?php echo base_url();?>control_graficas'
-			});
-		});
-		//Si se han enviado los datos
-		$('#submit_data').click(function(){
-			var n_errors = $('#numero_errores').val();
-			var error_list = CKEDITOR.instances['lista_errores'].getData();
-			var n_type_error = $('#numero_tipos_error').val();
-			var type_list = CKEDITOR.instances['lista_tipos_error'].getData();
-			var date = $('#datepicker').datepicker({ dateFormat: 'dd-mm-yy' }).val();
-			var num_ecuation = $('#select_ecuation').val();
-			var num_metrica =$('#select_metrica').val();
-			$.ajax({
-				type: "post",
-				url: '<?php echo base_url();?>registro_error/salvar_datos',
-				data: {
-					fecha: date,
-					num_errors: n_errors,
-					list_error: error_list,
-					num_type_errors: n_type_error,
-					list_type_error: type_list,
-					ecuation: num_ecuation,
-					metric: num_metrica
-				},
-				success: function (response){
-					if (response == 1){
-						alert ('Se guardo el registro correctamente');
-						emptyAll();
-					} else {
-						alert ('ocurrio un error al guardar datos con el servidor');
-						emptyAll();
-					}
-				},
-				error: function (){
-					alert('Faltan campos por completar');
-				}
-			});
+    $(document).ready (function(){
+        //Convert value to datepicker
+        $('#datepicker').datepicker();
+        //Cambiando con ckeditor
+        CKEDITOR.replace("lista_errores");
+        CKEDITOR.replace("lista_tipos_error");
+        CKEDITOR.config.width = 700
+        //Generar opciones para las ecuaciones
+        //crearOpcionEcuaciones();
 
-		});
+        //AQUI VA EL CODIGO PARA REGRESAR O SALIR DEL SISTEMA
+        $('#back_button').click(function(){
+            alert('Regresando pero a la misma pagina');
+        });
+        $('#grafica_button').click(function(){
+            $.ajax({
+                type: "post",
+                url: '<?php echo base_url();?>control_graficas'
+            });
+        });
 
-	function emptyAll(){
-
-	}
-	function crearOpcionEcuaciones(){
-		$.ajax({
-			type: "post",
-			url: '<?php echo base_url();?>registro_error/obtener_ecuaciones',
-			success: function(response){
-				var html_code = "";
-				var ecuaciones = JSON.parse(response);
-				html_code +='<select id="select_ecuation">';
-				html_code += '<option value="0" disabled="disabled" selected="selected">(Seleccionar)</option>';
-				for (var i=0; i < ecuaciones.length;i++){
-					html_code+='<option value="'+ecuaciones[i]['numero']+'">'+ecuaciones[i]['numero']+': '+ecuaciones[i]['cadena']+'</option>';
-				}
-				html_code += '</select>';
-				$('#select_ecuacion_placeholder').html (html_code);
-			},
-			error: function(){
-				alert('Error found');
-			}
-		});
-	}
-	</script>
+    });
+    
+    function crearOpcionEcuaciones(){
+        $.ajax({
+            type: "post",
+            url: '<?php echo base_url();?>registro_error/obtener_ecuaciones',
+            success: function(response){
+                var html_code = "";
+                var ecuaciones = JSON.parse(response);
+                html_code +='<select id="select_ecuation">';
+                html_code += '<option value="0" disabled="disabled" selected="selected">(Seleccionar)</option>';
+                for (var i=0; i < ecuaciones.length;i++){
+                    html_code+='<option value="'+ecuaciones[i]['numero']+'">'+ecuaciones[i]['numero']+': '+ecuaciones[i]['cadena']+'</option>';
+                }
+                html_code += '</select>';
+                $('#select_ecuacion_placeholder').html (html_code);
+            },
+            error: function(){
+                alert('Error found');
+            }
+        });
+    }
+    </script>
+</html>
--- database/registro.sql
+++ database/registro.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.6deb1
+-- version 4.1.12
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 27-11-2014 a las 17:57:28
--- Versión del servidor: 5.5.40-0ubuntu1
--- Versión de PHP: 5.5.12-2ubuntu4.1
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-11-2014 a las 22:21:32
+-- Versión del servidor: 5.6.16
+-- Versión de PHP: 5.5.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,47 +27,25 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `registro` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `numero_metrica` int(11) NOT NULL,
-  `numero_ecuacion` int(11) NOT NULL,
+  `ecuacion` varchar(80) NOT NULL,
   `errores_encontrados` int(11) NOT NULL,
   `tipos_encontrados` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `lista_errores` varchar(2000) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
-  `lista_tipos` varchar(2000) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+  `lista_tipos` varchar(2000) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Volcado de datos para la tabla `registro`
 --
 
-INSERT INTO `registro` (`id`, `numero_metrica`, `numero_ecuacion`, `errores_encontrados`, `tipos_encontrados`, `fecha`, `lista_errores`, `lista_tipos`) VALUES
-(7, 1, 1, 5, 2, '2014-11-27', '<p>asdasd</p>\n', '<p>asdasd</p>\n'),
-(8, 1, 1, 15, 2, '2014-11-28', '<p>asdasd</p>\n', '<p>asdasd</p>\n'),
-(9, 2, 0, 15, 6, '2014-11-28', '<p>das</p>\n', '<p>asd</p>\n'),
-(10, 1, 1, 5, 6, '2014-11-28', '<p>asd</p>\n', '<p>as</p>\n'),
-(11, 2, 2, 5, 6, '2014-11-28', '<p>asd</p>\n', '<p>sadas</p>\n'),
-(12, 1, 1, 10, 5, '2014-11-28', '<p>nhjdshdsadhsg</p>\n', '<p>hjsdghgsdahjghsdhj</p>\n\n<p>hjadshjdsahdsjakjk</p>\n');
+INSERT INTO `registro` (`id`, `numero_metrica`, `ecuacion`, `errores_encontrados`, `tipos_encontrados`, `fecha`, `lista_errores`, `lista_tipos`) VALUES
+(9, 1, 'x=y', 3, 2, '2014-11-29', '<p>bbb</p>\n', '<p>d,d</p>\n'),
+(10, 2, 'x=y', 1, 1, '2014-11-30', '<p>bbb</p>\n', '<p>n</p>\n');
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `registro`
---
-ALTER TABLE `registro`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `registro`
---
-ALTER TABLE `registro`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
