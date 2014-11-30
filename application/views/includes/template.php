96e41891fcc9f425be61adafc999d6713e8f6e03
shk33 <m.coronel.seg@gmail.com>
2014-11-30 12:40:03 -0600

    Remove Morris Chart js reference from the main template

--- application/views/includes/template.php
+++ application/views/includes/template.php
@@ -37,10 +37,6 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
 
-    <!-- Morris Charts JavaScript -->
-    <script src="<?php echo base_url(); ?>js/plugins/morris/raphael.min.js"></script>
-    <script src="<?php echo base_url(); ?>js/plugins/morris/morris.min.js"></script>
-    <script src="<?php echo base_url(); ?>js/plugins/morris/morris-data.js"></script>
 
 </head>
 
